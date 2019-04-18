1bcedad4296691c1d571569788a21a2c14b0cb50
林有勋 <linyouxun@yoju360.com>
2019-04-18 15:36:44 +0800

    [修改]提示信息

--- src/Home.tsx
+++ src/Home.tsx
@@ -13,7 +13,7 @@ class Home extends React.Component<any, any> {
     Modal.warn({
       width: 600,
       title: <div>各位大佬，现在账号有点错乱，数据有点对应不上了，建议各自使用自己的账号登陆</div>,
-      content: <img style={{width: '100%'}} src="http://localhost:3100/accout.png" alt=""/>,
+      content: <img style={{width: '100%'}} src="http://47.106.174.88/accout.png" alt=""/>,
       onCancel: () => {
         self.props.history.push('/active/list');
       },
@@ -33,13 +33,18 @@ class Home extends React.Component<any, any> {
         各位<span>大佬</span>，现在<span>账号有点错乱</span>，数据有点对应不上了，<span>建议</span>各自使用<span>自己</span>的<span>账号登陆</span>
         {/* <img src="http://localhost:3100/ku.jpg" alt=""/> */}
       </h4>
-      <img src="http://localhost:3100/accout.png" alt=""/>
+      <img src="http://47.106.174.88/accout.png" alt=""/>
       <br/>
       <br/>
       <h4>如有遗漏，纯属巧合</h4>
       <pre>
+        本次更新更新：
+          1.基本信息添加<span>上报ID</span>;
+          2.添加新控件跳转了<span>链接按钮</span>
+      </pre>
+      <pre>
         下版预计更新：
-          1.单点登录;限制账号同时多点登录，多用户使用同一账号;
+          1.<span>单点登录</span>;限制账号同时多点登录，多用户使用同一账号;
           2.新功能开发
       </pre>
     </div>)
